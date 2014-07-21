57d1d420ade107e3a9d30808d732684bc3985655
Dejan Bosanac <dejan@nighttale.net>
2014-07-21 19:01:20 +0200

    add schema generation and validation

--- hornetq-bootstrap/src/main/java/org/hornetq/cli/commands/Run.java
+++ hornetq-bootstrap/src/main/java/org/hornetq/cli/commands/Run.java
@@ -20,9 +20,14 @@ import org.hornetq.factory.CoreFactory;
 import org.hornetq.factory.SecurityManagerFactory;
 import org.hornetq.spi.core.security.HornetQSecurityManager;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 import java.io.File;
+import java.io.InputStream;
 
 @Command(name = "run", description = "runs the broker instance")
 public class Run implements Action
@@ -35,6 +40,12 @@ public class Run implements Action
       File file = new File("config/hornetq.xml");
 
       Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+      SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+      sf.setFeature("http://apache.org/xml/features/validation/schema-full-checking", false);
+      InputStream xsdStream = Run.class.getClassLoader().getResourceAsStream("org/hornetq/dto/hornetq.xsd");
+      StreamSource xsdSource = new StreamSource(xsdStream);
+      Schema schema = sf.newSchema(xsdSource);
+      unmarshaller.setSchema(schema);
 
       BrokerDTO broker = (BrokerDTO) unmarshaller.unmarshal(file);
 
