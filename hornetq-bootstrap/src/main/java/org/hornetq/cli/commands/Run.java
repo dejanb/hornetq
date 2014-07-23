d2480e03d75578c2beaab7e26fef3c70a37b8827
Dejan Bosanac <dejan@nighttale.net>
2014-07-23 13:30:02 +0200

    allow to specify custom configuration file and different configuration formats

--- hornetq-bootstrap/src/main/java/org/hornetq/cli/commands/Run.java
+++ hornetq-bootstrap/src/main/java/org/hornetq/cli/commands/Run.java
@@ -12,48 +12,39 @@
  */
 package org.hornetq.cli.commands;
 
+import io.airlift.command.Arguments;
 import io.airlift.command.Command;
 import org.hornetq.core.config.Configuration;
 import org.hornetq.core.server.impl.HornetQServerImpl;
 import org.hornetq.dto.BrokerDTO;
+import org.hornetq.factory.BrokerFactory;
 import org.hornetq.factory.CoreFactory;
 import org.hornetq.factory.SecurityManagerFactory;
 import org.hornetq.spi.core.security.HornetQSecurityManager;
+import org.hornetq.utils.FactoryFinder;
 
-import javax.xml.XMLConstants;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.Unmarshaller;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
-import java.io.File;
-import java.io.InputStream;
+import java.net.URI;
 
 @Command(name = "run", description = "runs the broker instance")
 public class Run implements Action
 {
 
+   @Arguments(description = "Broker Configuration URI, default 'xml:config/hornetq.xml'")
+   String configuration = "xml:config/hornetq.xml";
+
    @Override
    public Object execute(ActionContext context) throws Exception
    {
-      JAXBContext jaxbContext = JAXBContext.newInstance("org.hornetq.dto");
-      File file = new File("config/hornetq.xml");
+      URI configURI = new URI(configuration);
+      FactoryFinder finder = new FactoryFinder("META-INF/services/org/hornetq/broker/");
+      BrokerFactory factory = (BrokerFactory)finder.newInstance(configURI.getScheme());
+      BrokerDTO broker = factory.createBroker(configURI);
 
-      Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-      SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-      sf.setFeature("http://apache.org/xml/features/validation/schema-full-checking", false);
-      InputStream xsdStream = Run.class.getClassLoader().getResourceAsStream("org/hornetq/dto/hornetq.xsd");
-      StreamSource xsdSource = new StreamSource(xsdStream);
-      Schema schema = sf.newSchema(xsdSource);
-      unmarshaller.setSchema(schema);
-
-      BrokerDTO broker = (BrokerDTO) unmarshaller.unmarshal(file);
-
-      Configuration configuration = CoreFactory.create(broker.core);
+      Configuration core = CoreFactory.create(broker.core);
 
       HornetQSecurityManager security = SecurityManagerFactory.create(broker.security);
 
-      HornetQServerImpl server = new HornetQServerImpl(configuration, security);
+      HornetQServerImpl server = new HornetQServerImpl(core, security);
       server.start();
 
       return null;
